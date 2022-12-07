5c8117d9706cfc70abae517d27085fb8c07dee16
Sarah Awik <sarah@sarahaweek.sarahawik>
2022-12-07 09:38:35 +0200

    Step_2

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,5 @@
+var name ="Sarah";
+var surname = "Awik";
+var city = "akkar";
+
+alert ("Name :"+ name + "\n"+"Surname :"+ surname+ "\n"+"City :"+city );
--- javascript-basics-01/Step_3.js
+++ javascript-basics-01/Step_3.js
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Step 2</title>
+        <title>Step 3</title>
         <meta charset="UTF-8">
-        <script src="Step_2.js"></script>
     </head>
     <body>
     </body>
