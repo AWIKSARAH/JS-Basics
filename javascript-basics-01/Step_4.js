b70861c2c011d20c93aa0a0f73a65e955cd05c3b
Sarah Awik <sarah@sarahaweek.sarahawik>
2022-12-07 09:51:31 +0200

    Step_4

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Step 4</title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        <form>
-            <label for="name">name : </label><input type="text" name="name" id="name"/>
-            <label for="surname">surname : </label><input type="text" name="surname" id="surname"/>
-            <label for="city">city : </label><input type="text" name="city" id="city"/>
-            <button  name="validate" id="validate">Validate</button>
-        </form>
-    </body>
-</html>
+var name=prompt("What is your First Name");
+var LastName=prompt("What is your Last Name");
+var City=prompt("What is your City");
+
+alert ("Name :"+ name + "\n"+"Surname :"+ LastName+ "\n"+"City :"+City );
