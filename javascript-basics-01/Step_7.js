0a921944884fa733a4876ac9ea6cb2415a702d94
Sarah Awik <sarah@sarahaweek.sarahawik>
2022-12-08 16:41:51 +0200

    Step_7_01

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,6 +1,7 @@
 var shoe_size = prompt("How match size");
 var birth_year = prompt("What is your birth year");
-
+document.getElementById('shoe_size').value=shoe_size;
+document.getElementById('year').value=birth_year;
 function myFunction() {
     var cal = shoe_size*2;
     cal=cal+5;
@@ -9,4 +10,8 @@ function myFunction() {
     cal=cal+1766;
     return cal;
   }
-  alert (myFunction());
+  //alert (myFunction());
+  document.getElementById("validate").addEventListener("click",function(){
+    alert(myFunction())
+    }
+    )
