c10db1e9a1d5d4d1fb9966c8ffa4db4105098a0b
Sarah Awik <sarah@sarahaweek.sarahawik>
2022-12-08 11:33:10 +0200

    Step_2_03

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,12 @@
+// var a=;
+console.log("im here")
+
+document.getElementById("hide").addEventListener("click", function(){
+  
+    document.getElementById("texte").style = 'display: none';
+});
+
+document.getElementById("show").addEventListener("click", function(){
+  
+    document.getElementById("texte").style = 'display: block';
+});
---
+++ javascript-basics-03/step 4/Step_4.js
@@ -0,0 +1,8 @@
+// var a=;
+console.log("im here")
+
+document.getElementById("image1").addEventListener("mouseover", function(){
+  
+    this.style = 'border:10px solid red';
+});
+
