9d5e005da93badaa922492fe8e5b6515726efd08
Sarah Awik <sarah@sarahaweek.sarahawik>
2022-12-08 16:46:45 +0200

    Step_8_01

--- javascript-basics-01/Step_8.js
+++ javascript-basics-01/Step_8.js
@@ -1,7 +1,19 @@
 var age=prompt("How old are you");
+document.getElementById('age').value=age;
+document.getElementById("validate").addEventListener("click",function(){
+
+if (age > 18) {
+  alert("you are over 18");
+}
+else{
+  alert("you are under 18" );
+}
+  //alert(Fnum%Snum)
+
 if (age > 18) {
     alert("you are over 18");
   }
   else{
     alert("you are under 18" );
   }
+});
